b6ae3764e6212adc946ae636e0c54c9880f0dfbc
Tirade6 <tmauchamp.0@gmail.com>
2025-07-21 17:44:25 +0200

    Test of the linting badge

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-def calculate_average(numbers):
-    total = sum(numbers)
-    count = len(numbers)
-    average = total / count
-    print(average)
-
-result = calculate_average([1, 2, 3, 4, 5])
+print('hello World !')
